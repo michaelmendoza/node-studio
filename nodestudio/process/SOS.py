3ab3479b53fcb677c41ba73023d1b5891c8745e9
ellieYD <yokidoke607@gmail.com>
2022-03-13 16:47:16 +0000

    sos output data still not right

--- nodestudio/process/SOS.py
+++ nodestudio/process/SOS.py
@@ -1,13 +1,16 @@
 import numpy as np
+from process.reformat import reformat
 
 def sum_of_squares(a,b):
     if isinstance(a, np.ndarray) & isinstance(b, np.ndarray):
-        result = (a**2+b**2)**0.5
+        result = a**2+b**2
+
+        #result = reformat(result)
 
         min = np.min(result)
         max = np.max(result)
-        #resolution = 4096
-        result = (result - min) / (max - min)
+        resolution = 4096
+        result = (result - min) * resolution / (max - min)
         result = np.floor(result).astype('uint16')
 
         return result
--- nodestudio/process/mock.py
+++ nodestudio/process/mock.py
@@ -1,4 +1,5 @@
 import numpy as np
+from process.reformat import reformat
 
 def mock_2d_data(pattern):
     xdim = 50
@@ -15,13 +16,14 @@ def mock_2d_data(pattern):
             for i in range(xdim-1):
                 data[j,i] = ((j+1-ydim/2)**2+(i+1-xdim/2)**2)**0.5
 
-    data = np.reshape(data,(1,data.shape[0],data.shape[1]))
+    data = reformat(data)
+    # data = np.reshape(data,(1,data.shape[0],data.shape[1]))
 
-    min = np.min(data)
-    max = np.max(data)
-    scaled_data = (data - min) / (max - min)
-    resolution = 4096
-    data = np.floor(scaled_data * resolution).astype('uint16')
+    # min = np.min(data)
+    # max = np.max(data)
+    # scaled_data = (data - min) / (max - min)
+    # resolution = 4096
+    # data = np.floor(scaled_data * resolution).astype('uint16')
 
     return data
 
