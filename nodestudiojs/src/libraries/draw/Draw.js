92a520908e970efc3754bfcc6ad1ef8ce90d113b
ellieYD <yokidoke607@gmail.com>
2022-03-13 15:38:33 +0000

    added basic colormap, tested

--- nodestudiojs/src/libraries/draw/Draw.js
+++ nodestudiojs/src/libraries/draw/Draw.js
@@ -1,3 +1,26 @@
+function getColor(vin){
+    //refence: http://paulbourke.net/miscellaneous/colourspace/
+    const color = [];
+
+    if(vin < 0.25){
+        color[0] = 0;
+        color[1] = 4 * vin * 255;
+        color[2] = 255;
+    }else if (vin < 0.5){
+        color[0] = 0
+        color[1] = 255;
+        color[2] = 255 + 4 * (0.25 - vin) * 255;
+    }else if (vin < 0.75){
+        color[0] = 4 * (vin - 0.5) * 255;
+        color[1] = 255;
+        color[2] = 0;
+    }else {
+        color[0] = 255;
+        color[1] = 255 + 4 * (0.75 - vin) * 255;
+        color[2] = 0;
+    }
+    return color;
+}
 
 export const DrawImg = (data) => {
 
@@ -7,20 +30,33 @@ export const DrawImg = (data) => {
     const width = data.shape[1];
 
     const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');		
+    const context = canvas.getContext('2d');	
     canvas.width =  width;
     canvas.height = height;
     const imageData = context.getImageData(0, 0, width, height);
 
+    // B/W
+    // let i = 0;
+    // for(let y = 0; y < height; y++)
+    //     for(let x = 0; x < width; x++, i++) {
+    //         const value = pixelArray[ y * width + x ] * 255 / resolution;
+    //         imageData.data[4*i] = value;
+    //         imageData.data[4*i+1] = value;
+    //         imageData.data[4*i+2] = value;
+    //         imageData.data[4*i+3] = 255;
+    //     }
+
+    // RGB colormap
     let i = 0;
     for(let y = 0; y < height; y++)
         for(let x = 0; x < width; x++, i++) {
-            const value = pixelArray[ y * width + x ] * 255 / resolution;
-            imageData.data[4*i] = value;
-            imageData.data[4*i+1] = value;
-            imageData.data[4*i+2] = value;
+            const value = pixelArray[ y * width + x ] / resolution;
+            imageData.data[4*i] = getColor(value)[0];
+            imageData.data[4*i+1] = getColor(value)[1];
+            imageData.data[4*i+2] = getColor(value)[2];
             imageData.data[4*i+3] = 255;
         }
+
     
     context.putImageData(imageData, 0, 0);
     const dataURL = canvas.toDataURL();    
