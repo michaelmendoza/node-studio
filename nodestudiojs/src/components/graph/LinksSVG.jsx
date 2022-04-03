ca205b549e75b5be8f7fa4fbe8a856630dd41834
Michael Mendoza <askmichaelmendoza@gmail.com>
2022-04-03 13:35:48 -0400

    Refactored NewLink

--- nodestudiojs/src/components/graph/LinksSVG.jsx
+++ nodestudiojs/src/components/graph/LinksSVG.jsx
@@ -13,18 +13,18 @@ const dyOffset = 34 * scale;
 const Port = ({port, index, type, handleMouseDown, handleMouseUp}) => {
 
     const [radius, setRadius] = useState(5);
-    const [fill, setFill] = useState("#FEFEFE");
-    const [stroke, setStroke] = useState("#444444");
+    const [fill, setFill] = useState('#FEFEFE');
+    const [stroke, setStroke] = useState('#444444');
 
-    const handleMouseEnter = () => { setRadius(6); setStroke("#000000"); }
+    const handleMouseEnter = () => { setRadius(6); setStroke('#000000'); }
 
-    const handleMouseLeave = () => { setRadius(5); setStroke("#444444"); }
+    const handleMouseLeave = () => { setRadius(5); setStroke('#444444'); }
 
     return (
         <g className='port' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} 
                             onMouseDown={(e) => handleMouseDown(e, type, index)} onMouseUp={(e) => handleMouseUp(e, port.node, type, index)}>
             <circle className='port-circle' cx={port.x} cy={port.y} r={radius} fill={fill} stroke={stroke}></circle> 
-            <rect className='port-clickbox' x={port.x - 14} y={port.y - 13} width="26" height="26" opacity="0%"></rect> 
+            <rect className='port-clickbox' x={port.x - 14} y={port.y - 13} width='26' height='26' opacity='0%'></rect> 
         </g>
     )
 }
@@ -32,7 +32,7 @@ const Port = ({port, index, type, handleMouseDown, handleMouseUp}) => {
 const Link = ({link, onContextMenu}) => {
     const {state, dispatch} = useContext(AppState.AppContext);
 
-    const [line, setLine] = useState({ stroke0:"#222222", stroke1:"#FEFEFE", opacity0:"0.4", opacity1:"1.0" });
+    const [line, setLine] = useState({ stroke0:'#222222', stroke1:'#FEFEFE', opacity0:'0.4', opacity1:'1.0' });
 
     const startNodeID = link.startNode;
     const startNode = state.nodes[startNodeID];
@@ -48,9 +48,9 @@ const Link = ({link, onContextMenu}) => {
         dispatch({ type: ActionTypes.SET_ACTIVE_ELEMENT, activeElement:link });
     }
 
-    const handleMouseEnter = () => { setLine({...line, opacity0:"0.8", stroke0:"#222222", stroke1:"#EEEEEE", opacity1:"0.8"}); }
+    const handleMouseEnter = () => { setLine({...line, opacity0:'0.8', stroke0:'#222222', stroke1:'#EEEEEE', opacity1:'0.8'}); }
 
-    const handleMouseLeave = () => { setLine({...line, opacity0:"0.4", stroke0:"#222222", stroke1:"#FEFEFE", opacity1:"1.0"}); }
+    const handleMouseLeave = () => { setLine({...line, opacity0:'0.4', stroke0:'#222222', stroke1:'#FEFEFE', opacity1:'1.0'}); }
 
     const handleContextMenu = e => { 
         e.preventDefault();
@@ -60,11 +60,26 @@ const Link = ({link, onContextMenu}) => {
     // Note: To figureout svg path: https://sean.brunnock.com/SVG/SVGPathGenerator/
 
     return (
-        <g className={'link-' + link.id} onClick={handleClick} onContextMenu={handleContextMenu} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-            <path d={`M${p1.x},${p1.y} C${(p2.x+p1.x)/2},${p1.y} ${(p2.x+p1.x)/2},${p2.y} ${p2.x},${p2.y}`} strokeWidth={4} stroke={line.stroke0} strokeLinejoin={"round"} strokeOpacity={line.opacity0} fillOpacity="0"/>
-            <path d={`M${p1.x},${p1.y} C${(p2.x+p1.x)/2},${p1.y} ${(p2.x+p1.x)/2},${p2.y} ${p2.x},${p2.y}`} strokeWidth={2} stroke={line.stroke1} strokeLinejoin={"round"} strokeOpacity={line.opacity1} fillOpacity="0"/>
-            <circle cx={p1.x} cy={p1.y} r="3" fill={"#444444"}></circle>
-            <circle cx={p2.x} cy={p2.y} r="3" fill={"#444444"}></circle>
+        <g className={'link-line link-' + link.id} onClick={handleClick} onContextMenu={handleContextMenu} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <path d={`M${p1.x},${p1.y} C${(p2.x+p1.x)/2},${p1.y} ${(p2.x+p1.x)/2},${p2.y} ${p2.x},${p2.y}`} strokeWidth={4} stroke={line.stroke0} strokeLinejoin={'round'} strokeOpacity={line.opacity0} fillOpacity='0'/>
+            <path d={`M${p1.x},${p1.y} C${(p2.x+p1.x)/2},${p1.y} ${(p2.x+p1.x)/2},${p2.y} ${p2.x},${p2.y}`} strokeWidth={2} stroke={line.stroke1} strokeLinejoin={'round'} strokeOpacity={line.opacity1} fillOpacity='0'/>
+            <circle cx={p1.x} cy={p1.y} r='3' fill={'#444444'}></circle>
+            <circle cx={p2.x} cy={p2.y} r='3' fill={'#444444'}></circle>
+        </g>
+    )
+}
+
+const NewLink = ({activePort, position}) => {
+    const p1 = activePort.type === 'output' ? { x: activePort.node.position.x + startOffset.x, y: activePort.node.position.y + startOffset.y + activePort.index * dyOffset } :
+                                        { x: activePort.node.position.x + endOffset.x, y: activePort.node.position.y + endOffset.y + activePort.index * dyOffset }
+    const p2 = { x: position.x, y: position.y }
+    const line = { stroke0:'#222222', stroke1:'#FEFEFE', opacity0:'0.4', opacity1:'1.0' };
+
+    return (
+        <g className='render-new-line'>
+            <path d={`M${p1.x},${p1.y} C${(p2.x+p1.x)/2},${p1.y} ${(p2.x+p1.x)/2},${p2.y} ${p2.x},${p2.y}`} strokeWidth={4} stroke={line.stroke0} strokeLinejoin={'round'} strokeOpacity={line.opacity0} fillOpacity='0'/>
+            <path d={`M${p1.x},${p1.y} C${(p2.x+p1.x)/2},${p1.y} ${(p2.x+p1.x)/2},${p2.y} ${p2.x},${p2.y}`} strokeWidth={2} stroke={line.stroke1} strokeLinejoin={'round'} strokeOpacity={line.opacity1} fillOpacity='0'/>
+            <circle cx={p1.x} cy={p1.y} r='3' fill={'#444444'}></circle>
         </g>
     )
 }
@@ -104,22 +119,8 @@ const LinksSVG = ({position, onContextMenu, width = 1600, height = 1600}) => {
         )
     }
 
-    const renderNewLine = () => {
-        const p1 = activePort.type === 'output' ? { x: activePort.node.position.x + startOffset.x, y: activePort.node.position.y + startOffset.y + activePort.index * dyOffset } :
-                                            { x: activePort.node.position.x + endOffset.x, y: activePort.node.position.y + endOffset.y + activePort.index * dyOffset }
-        const p2 = { x: position.x, y: position.y }
-
-        return (
-            <g className='render-new-line'>
-                <line x1={p1.x} y1={p1.y} x2={p2.x} y2={p2.y} strokeWidth={3} stroke={"#FEFEFE"} strokeLinejoin={"round"} strokeOpacity="0.5"></line>
-                <line x1={p1.x} y1={p1.y} x2={p2.x} y2={p2.y} strokeWidth={2} stroke={"#444444"} strokeLinejoin={"round"} strokeOpacity="0.9"></line>
-                <circle cx={p1.x} cy={p1.y} r="3" fill={"#444444"}></circle>
-            </g>
-        )
-    }
-
     return (
-        <div className="links-svg">
+        <div className='links-svg'>
             <svg height={height} width={width}>
             {
                 Object.values(state.nodes).map(node => node ? renderPorts(node) : null)
@@ -128,7 +129,7 @@ const LinksSVG = ({position, onContextMenu, width = 1600, height = 1600}) => {
                 state.links.map(link => <Link key={link.id} link={link} onContextMenu={onContextMenu}></Link>)
             }
             {
-                state.mouseState === MouseStates.CREATE_LINK ? renderNewLine() : null
+                state.mouseState === MouseStates.CREATE_LINK ? <NewLink activePort={activePort} position={position}></NewLink> : null
             }
             </svg>
         </div>
