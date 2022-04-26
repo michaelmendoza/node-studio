797ccde90c2db9558908e6efef5e1f0d6fedaaa7
Zimu Huo <92516705+ZimuHuo@users.noreply.github.com>
2022-04-26 20:58:30 +0100

    Create fft.py
    This fft deals with high dimensional data

---
+++ nodestudio/process/fft.py
@@ -0,0 +1,16 @@
+import numpy as np
+# this deals with high dimensional data, eg. [y, x, coil, slice]
+
+def ifft2c(F, axis = (0,1)):
+    x,y = (axis)
+    tmp0 = np.fft.ifftshift(np.fft.ifftshift(F, axes=(x,)), axes=(y,))
+    tmp1 = np.fft.ifft(np.fft.ifft(tmp0, axis = x), axis = y)
+    f = np.fft.fftshift(np.fft.fftshift(tmp1, axes=(x,)), axes=(y,))
+    return f   
+
+def fft2c(f, axis = (0,1)):
+    x,y = (axis)
+    tmp0 = np.fft.fftshift(np.fft.fftshift(f, axes=(x,)), axes=(y,))
+    tmp1 = np.fft.fft(np.fft.fft(tmp0, axis = x), axis = y)
+    F = np.fft.ifftshift(np.fft.ifftshift(tmp1, axes=(x,)), axes=(y,))
+    return F   
