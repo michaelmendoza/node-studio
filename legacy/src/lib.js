60eefed270afc4c194a25bfc59af11442e8fb9ff
Michael Mendoza <askmichaelmendoza@gmail.com>
2022-04-29 07:28:30 -0400

    Removed legacy code

--- legacy/src/lib.js
+++
@@ -1,6 +0,0 @@
-
-console.log('ImageNodes -', 'Version: 0.0.22', 'Date:Feb 13, 2020');
-
-import Graph from './graph/graph';
-
-export default Graph;
--- legacy/src/mr-graph/index.js
+++
@@ -1,2 +0,0 @@
-
-export { default as Graph } from "./components/Graph"
