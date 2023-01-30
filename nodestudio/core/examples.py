6e745bea6ffe240132f6b810fb52281c0895a96f
Michael Mendoza <askmichaelmendoza@gmail.com>
2023-01-30 14:59:33 +0000

    Updated Downloading DOSMA Example.

--- nodestudio/core/examples.py
+++ nodestudio/core/examples.py
@@ -1,9 +1,25 @@
 import os
 import gdown
 import zipfile
+from core import io
+
+def load_example_data():
+    ''' Loads example file data into memory '''
+    filepath_example = os.path.join(os.getcwd(), 'data', 'examples', 'example1', '')
+    files = io.read_file(filepath_example, 'example1-b7027ec6f5b311ecbc2eacde48001122') #'./data/examples/example1'
+    print('Example 1 data loaded: ' + files)
+
+    filepath_dess = os.path.join(os.getcwd(), 'data', 'examples', 'example2', 'healthy07-anonymized','qdess', '')
+    files = io.read_file(filepath_dess, 'example2-b7027ec6f5b311ecbc2eacde48001123') #'./data/examples/example2/healthy07-anonymized'
+    print('Example 2 (DESS) data loaded: ' + files)
 
 def download_example_data():
-    # Downloads and extracts examples data for nodestudio from google drive location
+    ''' Downloads and extracts example data for nodestudio '''
+    download_example1_data()
+    download_dosma_example_data()
+
+def download_example1_data():
+    ''' Downloads and extracts example data for nodestudio from google drive location '''
 
     # Checks if example 1 data folder exists
     example_path = "./data/examples/example1"
@@ -14,7 +30,7 @@ def download_example_data():
 
     # Downloads example 1 data
     output = './data/examples'
-    print('Downloading files ...')
+    print('Example 1: Downloading files ...')
     url = 'https://drive.google.com/drive/folders/1kdCB72bqZSpQqxkrVND9ZmsQydSJBG3F?usp=sharing'
     gdown.download_folder(url, quiet=False, output=output)
     print('Download complete.')
@@ -28,19 +44,25 @@ def download_example_data():
         print('Extract complete.')
         print('Example 1 data located at: ./data/examples')
 
+def download_dosma_example_data():
+    ''' Downloads and extracts examples data for nodestudio for DOSMA '''
 
-
-
-def download_dess_data():
-    example_path = "./data/examples/healthy07-anonymized"
+    # Checks if example 1 data folder exists
+    example_path = "./data/examples/example2"
     example1Exists = os.path.exists(example_path)
     if example1Exists:
-        print('Nodestudio Examples: DESS data exists')
+        print('Nodestudio Examples: Example 2 (DOSMA) data exists')
         return
 
-    # Downloads example 1 data
-    output = './data/healthy07-anonymized'
-    print('Downloading files ...')
-    url = 'https://drive.google.com/drive/folders/1zMSF3jcOpS_Z_0P9mSAZDlLTXc58mK5A?usp=sharing'
-    gdown.download_folder(url, quiet=False, output=output)
+    print('Example 2 (DOSMA): Downloading files ...')
+    url = 'https://huggingface.co/datasets/arjundd/dosma-data/resolve/main/healthy07-knee-anonymized.zip'
+
+    import requests, zipfile, io
+    r = requests.get(url)
     print('Download complete.')
+
+    print('Extracting files ...')
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall("./data/examples/example2")
+    print('Extract complete.')
+    print('Example 2 data located at: ./data/examples')
