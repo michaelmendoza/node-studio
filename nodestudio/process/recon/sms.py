1048d5578cfdcfbc84afc6a428936681236fa88e
Zimu Huo <zimuhuo@outlook.com>
2023-02-05 19:25:16 +0000

    added 4 more different sms recon

--- nodestudio/process/recon/sms.py
+++ nodestudio/process/recon/sms.py
@@ -6,6 +6,10 @@ from core.datagroup import DataGroup
 from core.dataset import NodeDataset 
 from process.recon.SOS import * 
 from process.recon.sg import sg
+from process.recon.spsg import spsg
+from process.recon.sg2k import sg2k
+from process.recon.spsg2k import spsg2k
+
 import process.core.sms_sim as sms_sim
 def sms(data, ref, type):
     dataRset = np.copy(data.data)
@@ -32,9 +36,33 @@ def sms_recon(dataR, calib, type, kh = 2, kw = 3):
         recon = sms_sim.singleSliceFov(recon, - reconshift)
     if type == "SG":
         recon = ifft2c(sg(dataR, calib),(0,1))
-    
-    
-    
+
+    if type == "SPSG_CAIPI":
+        recon = ifft2c(spsg(dataR, calib),(0,1))
+        ny, nx, nc, ns = recon.shape
+        cycle = np.arange(0,1,1/ns) * 2* np.pi
+        reconshift = cycle*int(ns* ny/ns)/(2*np.pi) 
+        recon = sms_sim.singleSliceFov(recon, - reconshift)
+    if type == "SPSG":
+        recon = ifft2c(spsg(dataR, calib),(0,1))
+
+    if type == "SG2K_CAIPI":
+        recon = ifft2c(sg2k(dataR, calib),(0,1))
+        ny, nx, nc, ns = recon.shape
+        cycle = np.arange(0,1,1/ns) * 2* np.pi
+        reconshift = cycle*int(ns* ny/ns)/(2*np.pi) 
+        recon = sms_sim.singleSliceFov(recon, - reconshift)
+    if type == "SG2K":
+        recon = ifft2c(sg2k(dataR, calib),(0,1))
+
+    if type == "SPSG2K_CAIPI":
+        recon = ifft2c(spsg2k(dataR, calib),(0,1))
+        ny, nx, nc, ns = recon.shape
+        cycle = np.arange(0,1,1/ns) * 2* np.pi
+        reconshift = cycle*int(ns* ny/ns)/(2*np.pi) 
+        recon = sms_sim.singleSliceFov(recon, - reconshift)
+    if type == "SPSG2K":
+        recon = ifft2c(spsg2k(dataR, calib),(0,1))
 
 
     return rsos(np.moveaxis(recon, -1, 0))
